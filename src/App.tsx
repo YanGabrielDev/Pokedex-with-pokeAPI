50b4c761d963361bec71516c2d7869c88d1037d3
yangabrp <yg@forpeople.io>
2022-09-16 16:57:47 -0300

    implementação do roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import axios from "axios";
-import Pokemons from "./Services/Pokemons/getPokemons";
-import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react"
+import "./App.css"
+import axios from "axios"
+import Pokemons from "./Services/Pokemons/getPokemons"
+import { Grid } from "@mui/material"
+import { NavBar } from "./components/Navbar"
+import { TextInput } from "./components/Input"
+import { BroadcastChannel } from "worker_threads"
+import { BrowserRouter } from "react-router-dom"
+import { MainRoutes } from "./Routes/routes"
 
 function App() {
   return (
     <div className="App">
       <Grid container={true} display="center" justifyContent="center">
         <Grid lg={8}>
-          <Pokemons />
+          <BrowserRouter>
+            <NavBar />
+            <MainRoutes />
+          </BrowserRouter>
         </Grid>
       </Grid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
